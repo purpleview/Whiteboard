d56229562837855ed3b94181d83fd642ca01f0bd
Andreas Böhm <Andreas.Boehm@purpleview.de>
2025-11-19 07:39:41 +0100

    feat: Use default name "Benutzer nnnnn" instead of "unknownnnnnn".

--- src/js/main.js
+++ src/js/main.js
@@ -34,7 +34,7 @@ if (urlParams.get("whiteboardid") !== whiteboardId) {
     window.location.search = urlParams;
 }
 
-const myUsername = urlParams.get("username") || "unknown" + (Math.random() + "").substring(2, 6);
+const myUsername = urlParams.get("username") || "Benutzer " + (Math.random() + "").substring(2, 6);
 const accessToken = urlParams.get("accesstoken") || "";
 const copyfromwid = urlParams.get("copyfromwid") || "";
 
